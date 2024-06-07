316ddae78a36cc003b0817d0c81c93d83971f6dc
Nathan Green <n8green@gmail.com>
2024-06-07 11:48:50 -0600

    Updated runner script with copy to MM function

--- runner.js
+++ runner.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 const fs = require('node:fs');
+const { execSync } = require('child_process');
 const {envVars}  = require('/home/pi/Scripts/updater/env.js');
 
 let localPath = envVars.homePath+'/MagicMirror/gsversion.json';
 let remotePath = envVars.homePath+'/Scripts/updater/version.json';
+const mmPath = '/home/pi/Scripts/updater/assets/MagicMirror';
 const updaterVersion = fs.readFileSync(remotePath,'utf8');
 const localVersion = fs.readFileSync(localPath,'utf8');
 let updaterVersionData = JSON.parse(updaterVersion)
@@ -14,7 +16,8 @@ let localVersionData = JSON.parse(localVersion)
 
 if(updaterVersionData.about.version > localVersionData.about.version){
   console.log("New Version "+updaterVersionData.about.version+" is newer than installed version "+localVersionData.about.version)
-  copyFileDirectory();
+  let result = copyFileDirectory();
+  console.log(result);
   
 }
 else{
@@ -23,5 +26,14 @@ else{
 }
 
 function copyFileDirectory(){
-
+//copy files from updater directory to MagicMirror directory
+  let result = "error";
+  try{
+    let copyResult = execSync('cp -a '+mmPath+' '+homePath);
+    result = "Success: "+copyResult;   
+  }
+  catch(error){
+    result = "Error: "+error;
+  }
+  return result;
 }
