526a38c73be2b38fd300edb3cfec400c25e0ffce
Nathan Green <n8green@gmail.com>
2024-06-09 16:43:52 -0600

    Updated runner to increase version

--- runner.js
+++ runner.js
@@ -2,45 +2,75 @@
 const fs = require('node:fs');
 const { execSync } = require('child_process');
 const {envVars}  = require('/home/pi/Scripts/updater/env.js');
-const outputLog = fs.createWriteStream('./logs/runnerOutput.log',{flags:'a'});
+const outputLog = fs.createWriteStream(envVars.updaterFolder+'logs/runnerOutput.log',{flags:'a'});
 
-let localPath = envVars.homePath+'/MagicMirror/gsversion.json';
-let remotePath = envVars.homePath+'/Scripts/updater/version.json';
-const mmPath = '/home/pi/Scripts/updater/assets/MagicMirror';
-const updaterVersion = fs.readFileSync(remotePath,'utf8');
-const localVersion = fs.readFileSync(localPath,'utf8');
-let updaterVersionData = JSON.parse(updaterVersion)
-let localVersionData = JSON.parse(localVersion)
-//console.log(updaterVersionData);
-//console.log(localVersionData);
-//console.log("COMPARE: "+updaterVersionData.about.version +" - "+ localVersionData.about.version);
+let localVersionPath = envVars.homePath+'/MagicMirror/gsversion.json';
+let remoteVersionPath = envVars.updaterFolder+'version.json';
+const updatedFilesPath = envVars.updaterFolder+'assets/MagicMirror';
 
-let logMsg = "-----------------------"+new Date().toUTCString()+"-----------------------";
-console.log(logMsg);
-outputLog.write(logMsg + '\n');
-if(updaterVersionData.about.version > localVersionData.about.version){
+doUpdates();
 
-  let logMessage = "New Version "+updaterVersionData.about.version+" is newer than installed version "+localVersionData.about.version;
-  console.log(logMessage)
-  outputLog.write(logMessage + '\n');
-  let result = copyFileDirectory();
-  console.log(result);
-  outputLog.write(result + '\n');
+function doUpdates(){
+  let logMsg = "-----------------------"+new Date().toUTCString()+"-----------------------";
+  console.log(logMsg);
+  outputLog.write(logMsg + '\n');
+  let continueUpdate = false;
+  try{
+    const updaterVersionFile = fs.readFileSync(remoteVersionPath,'utf8');
+    const localVersionFile = fs.readFileSync(localVersionPath,'utf8');
+    let updaterVersionData = JSON.parse(updaterVersionFile);
+    let localVersionData = JSON.parse(localVersionFile);
 
-  //can I log to a file???????????/
+    if(updaterVersionData.about.version > localVersionData.about.version){
+
+      let logMessage = "New Version "+updaterVersionData.about.version+" is newer than installed version "+localVersionData.about.version;
+      console.log(logMessage)
+      outputLog.write(logMessage + '\n');
+      let result = copyFileDirectory();
+      console.log(result);
+      outputLog.write(result + '\n');
+      if(result.toLowerCase().indexOf("success")>-1)
+          continueUpdate = true;
+
+      //can I log to a file???????????/
+    }
+    else{
+      let logMessage = "New Version "+updaterVersionData.about.version+" is not newer than installed version "+localVersionData.about.version;
+      outputLog.write(logMessage + '\n');
+
+    }
+    if(continueUpdate){
+      let updatedVersion =  updateLocalVersion();
+      console.log(updatedVersion+ "running updateLocalVersion()");
+      outputLog.write(updatedVersion+ "running updateLocalVersion()");
+    }
+  }
+  catch(err){
+      console.log("Error running doUpdates(): "+err);
+      outputLog.write("Error running doUpdates(): "+err + '\n');
+  }
+
+  /** UPDATE THE LOCAL VERSION NUMBER OR ELSE THIS RUNS EVERY TIME */
 }
-else{
-  let logMessage = "New Version "+updaterVersionData.about.version+" is not newer than installed version "+localVersionData.about.version;
-  outputLog.write(logMessage + '\n');
 
-}
-/** BE SURE TO UPDATE THE LOCAL VERSION NUMBER OR ELSE THIS RUNS EVERY DAMN TIME */
 
 function copyFileDirectory(){
 //copy files from updater directory to MagicMirror directory
   let result = "error";
   try{
-    let copyResult = execSync('cp --verbose -a '+mmPath+' '+envVars.homePath);
+    let copyResult = execSync('cp --verbose -a '+updatedFilesPath+' '+envVars.homePath);
+    result = "Success: "+copyResult;   
+  }
+  catch(error){
+    result = "Error: "+error;
+  }
+  return result;
+}
+
+function updateLocalVersion(){
+  let result = "error";
+  try{
+    let copyResult = execSync('cp --verbose '+remoteVersionPath+' '+localVersionPath);
     result = "Success: "+copyResult;   
   }
   catch(error){
--- update.sh
+++ update.sh
@@ -1,29 +1,2 @@
 cd /home/pi/Scripts/updater
 node runner.js
-: '
-setTimeout(function(){
-    document.querySelector('div.top.center div.container').style.display = "block"
-    document.querySelector('div.top.center div.container').innerHTML = `<span id="calLoading">Loading...</span>`;
-},1000);
-
-setTimeout(function(){
-	document.querySelector('#calLoading').style.display = "none"
-},9500);
-
-TO DO
-1) Add a local version.txt to
-if(readmeDate>LocalReadMeDateOnServer--version.txt)
-
-version1
-
-if local is version 1 and new is version 3
-
-//if localVersion < = 1
-// do v1 update
-
-if localVersion <=2
-
-do v2 update
-
-etc...
-'
